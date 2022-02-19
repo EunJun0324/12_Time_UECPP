c53194dc00b2b2d22edf10671f81954d53601046
HongRin <72390797+HongRin@users.noreply.github.com>
2022-02-19 12:30:18 +0900

    0219

--- Source/UE_CPP_12/TPS/IRifle.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "TPS/IRifle.h"
-
-// Add default functionality here for any IIRifle functions that are not pure virtual.
--- Source/UE_CPP_12/UE_CPP_12.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE_CPP_12.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_CPP_12, "UE_CPP_12" );
